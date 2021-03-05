abe945f55d2a790676d2fa74099c9a054065081c
Nils Hoffmann <3309580+nilshoffmann@users.noreply.github.com>
2021-03-05 18:06:14 +0100

    Added keycloak configuration.

--- src/main/java/de/isas/lifs/palinom/webapp/config/WebSecurityConfig.java
+++ src/main/java/de/isas/lifs/palinom/webapp/config/WebSecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -36,6 +37,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  */
 @Configuration
 @EnableWebSecurity
+@Profile("!keycloak")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
